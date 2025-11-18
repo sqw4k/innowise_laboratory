6f47af3cae90c9a9819043e4150a14221eda9954
Sashnikov Alexey <asashnikov20017@gmail.com>
2025-11-19 00:35:49 +0300

    main.py finalized

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,45 +1,54 @@
 from datetime import datetime
 
+
 def generate_profile(age):
-    if age >= 0 and age <= 12:
+    if 0 <= age <= 12:
         return "Child"
-    elif age >= 13 and age <= 19:
+    elif 13 <= age <= 19:
         return "Teenager"
     else:
         return "Adult"
 
-user_name = input("Enter your full name: ")
-birth_year_str = input("Enter your birth year: ")
-birth_year = int(birth_year_str)
-now = datetime.now()
-if now.year - birth_year >= 135:
-    raise SystemExit("You entered the wrong year, are you still alive? Try again.")
-now = datetime.now()
-current_age = now.year - birth_year
-hobbies = []
-hobbie = ""
-hobbies_amount = 0
-while hobbie != "stop" :
-    hobbie = input("Enter a favourite hobby or type 'stop' to finish: ")
-    if hobbie == "stop":
-        break
-    hobbies.append(hobbie)
-    hobbies_amount += 1
-life_stage = generate_profile(current_age)
-user_profile = {
-    "user_name": user_name,
-    "age": current_age,
-    "life_stage": life_stage,
-    "Favourite hobbies": hobbies,
-}
-print(" \n--- \nProfile Summary:")
-print(f"Name: {user_profile['user_name']}")
-print(f"Age: {user_profile['age']}")
-print(f"Life Stage: {user_profile['life_stage']}")
-if hobbies_amount == 0:
-    print("You didn't mention any hobbies. \n ---")
-else:
-    print(f"Favourite Hobbies({hobbies_amount}): ")
-    for hobby in user_profile["Favourite hobbies"]:
-        print(f"- {hobby}")
-    print("---")
+
+def main():
+    print("Welcome to Mini-Profile Generator!")
+    user_name = input("Enter your full name: ")
+    birth_year_str = input("Enter your birth year: ")
+    birth_year = int(birth_year_str)
+
+    now = datetime.now()
+    if now.year - birth_year >= 135:
+        raise SystemExit("You entered the wrong year, are you still alive? Try again.")
+
+    current_age = now.year - birth_year
+
+    hobbies = []
+    hobby = ""
+    while True:
+        hobby = input("Enter a favourite hobby or type 'stop' to finish: ")
+        if hobby == "stop":
+            break
+        hobbies.append(hobby)
+
+    life_stage = generate_profile(current_age)
+    user_profile = {
+        "user_name": user_name,
+        "age": current_age,
+        "life_stage": life_stage,
+        "favourite_hobbies": hobbies,
+    }
+    print(" \n--- \nProfile Summary:")
+    print(f"Name: {user_profile['user_name']}")
+    print(f"Age: {user_profile['age']}")
+    print(f"Life Stage: {user_profile['life_stage']}")
+    if not hobbies:
+        print("You didn't mention any hobbies. \n ---")
+    else:
+        print(f"Favourite Hobbies({len(hobbies)}): ")
+        for hobby in user_profile["favourite_hobbies"]:
+            print(f"- {hobby}")
+        print("---")
+
+
+if __name__ == "__main__":
+    main()
